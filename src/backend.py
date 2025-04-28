2ff621654df289da171286636380c69ae5b8ddff
Alatin Sergey <alatin-sergeyy@rambler.ru>
2025-04-28 15:52:47 +0300

    Fix mistakes v2

--- src/backend.py
+++ src/backend.py
@@ -7,31 +7,38 @@ from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferMemory
 import asyncio
 import json
+from dotenv import load_dotenv
+import os
 
-llm = Ollama(model='mistral')
+load_dotenv()
+
+llm_url = os.getenv('LLM_URL')
+llm = Ollama(
+    model='mistral', 
+    base_url=llm_url,
+    )
 
 memory = ConversationBufferMemory()
-
 app = FastAPI()
 
 @app.post('/get_answer/')
 async def get_answer(item : Dict[str, Any]) -> Dict[str, Any]:
     logger.info(f'В LLM отправлено сообщение {item['text']}')
     template = """Говори на русском языке
-    
     {history}
     Human: {input}
     AI:"""
     prompt = PromptTemplate(
         input_variables=["history", "input"],
-        template=template
+        template=template,
     )
+
     conversation = ConversationChain(
         llm=llm,
         memory=memory,
         prompt=prompt,
         verbose=True,
     )
-    output = conversation.invoke({'input': item['text']})
 
+    output = conversation.invoke({'input': item['text']})
     return {'text': output['response']}
--- src/tg_bot.py
+++ src/tg_bot.py
@@ -1,14 +1,11 @@
 from aiogram.filters import CommandStart
-from aiohttp import web
-
+from aiohttp import web, ClientSession
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from loguru import logger
 from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from dotenv import load_dotenv
 import os
-
-from frontend_utils import answer_chat
 
 load_dotenv()
 
@@ -17,28 +14,39 @@ WEB_SERVER_HOST = os.getenv("WEB_SERVER_HOST")
 WEB_SERVER_PORT = int(os.getenv("WEB_SERVER_PORT"))
 WEBHOOK_PATH = os.getenv('WEBHOOK_PATH')
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+BACKEND_ENDPOINT = os.getenv('BACKEND_ENDPOINT')
 
 router = Router()
-
 bot = Bot(token=BOT_TOKEN)
 
 async def on_startup(bot: Bot) -> None:
-    webhook_url = f'{WEBHOOK_URL}{WEBHOOK_PATH}'
-    await bot.set_webhook(url=webhook_url,
-                           allowed_updates=router.resolve_used_update_types(),
-                           drop_pending_updates=True)
+    await bot.set_webhook(
+        url=f'{WEBHOOK_URL}{WEBHOOK_PATH}',
+        allowed_updates=router.resolve_used_update_types(),
+        drop_pending_updates=True,
+        )
+
 
 @router.message(CommandStart())
 async def start_command_handler(message : Message) -> None:
     await message.answer(f'Привет, {message.from_user.first_name}! Я работаю на вебхуках.')
 
+
 @router.message()
-async def echo_answer(message : Message) -> None:
-    results = await answer_chat(message)
-    try:
-        await message.answer(results)
-    except TypeError:
-        await message.answer("Nice try!")
+async def echo_answer(massege : Message) -> None:
+    data = {'text' : massege.text}
+    async with ClientSession().post(url=BACKEND_ENDPOINT, json=data) as response:
+        try:
+            response.raise_for_status()
+            response_data = await response.json()
+            logger.info(f'From LLM: {response_data}')
+            answer = response_data['text']
+        except Exception:
+            answer = response_data['text']
+        response.close()
+    await massege.answer(answer) 
+
+
 
 def main() -> None:
     dp = Dispatcher()
@@ -49,6 +57,7 @@ def main() -> None:
     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
     web.run_app(app, host='localhost', port=WEB_SERVER_PORT)
+
 
 if __name__ == "__main__":
     main()
